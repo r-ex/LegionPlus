20e4ccee4a84da2e00430bc18b1535236f8229c6
rexx <67599507+r-ex@users.noreply.github.com>
2022-12-16 18:19:51 +0000

    QC: remove unnecessary copying of structs

--- Legion/src/Assets/qc.cpp
+++ Legion/src/Assets/qc.cpp
@@ -8,35 +8,35 @@ void RpakLib::ExportQC(int assetVersion, const string& Path, const string& model
 	IO::StreamWriter qc(IO::File::Create(Path));
 	if (assetVersion <= 10)
 	{
-		s3studiohdr_t hdr = *reinterpret_cast<s3studiohdr_t*>(rmdlBuf);
+		s3studiohdr_t* hdr = reinterpret_cast<s3studiohdr_t*>(rmdlBuf);
 
 		qc.WriteFmt("$modelname \"%s\"\n", modelPath.ToCString());
 		qc.WriteFmt("$cdmaterials \"\"\n");
 
-		char* surfaceProp = reinterpret_cast<char*>(rmdlBuf + hdr.surfacepropindex);
+		char* surfaceProp = reinterpret_cast<char*>(rmdlBuf + hdr->surfacepropindex);
 
 		qc.WriteFmt("$surfaceprop \"%s\"\n", surfaceProp);
 
-		qc.WriteFmt("$contents \"%s\"\n\n", (hdr.contents & 1) == 1 ? "solid" : "notsolid");
+		qc.WriteFmt("$contents \"%s\"\n\n", (hdr->contents & 1) == 1 ? "solid" : "notsolid");
 
-		if (hdr.flags & STUDIOHDR_FLAGS_FORCE_OPAQUE)
+		if (hdr->flags & STUDIOHDR_FLAGS_FORCE_OPAQUE)
 			qc.Write("$opaque\n\n");
 
-		qc.WriteFmt("$eyeposition %f %f %f\n", hdr.eyeposition.X, hdr.eyeposition.Y, hdr.eyeposition.Z);
-		qc.WriteFmt("$illumposition %f %f %f\n\n", hdr.illumposition.X, hdr.illumposition.Y, hdr.illumposition.Z);
+		qc.WriteFmt("$eyeposition %f %f %f\n", hdr->eyeposition.X, hdr->eyeposition.Y, hdr->eyeposition.Z);
+		qc.WriteFmt("$illumposition %f %f %f\n\n", hdr->illumposition.X, hdr->illumposition.Y, hdr->illumposition.Z);
 
-		for (int i = 0; i < hdr.numbodyparts; ++i)
+		for (int i = 0; i < hdr->numbodyparts; ++i)
 		{
-			char* pBodyPart = rmdlBuf + hdr.bodypartindex + (i * sizeof(mstudiobodyparts_t));
-			mstudiobodyparts_t bodyPart = *reinterpret_cast<mstudiobodyparts_t*>(pBodyPart);
-			char* bodyPartName = reinterpret_cast<char*>(pBodyPart + bodyPart.sznameindex);
+			char* pBodyPart = rmdlBuf + hdr->bodypartindex + (i * sizeof(mstudiobodyparts_t));
+			mstudiobodyparts_t* bodyPart = reinterpret_cast<mstudiobodyparts_t*>(pBodyPart);
+			char* bodyPartName = reinterpret_cast<char*>(pBodyPart + bodyPart->sznameindex);
 
 			qc.WriteFmt("$bodygroup \"%s\"\n{\n", bodyPartName);
 
-			for (int j = 0; j < bodyPart.nummodels; ++j)
+			for (int j = 0; j < bodyPart->nummodels; ++j)
 			{
 				string baseModelName = IO::Path::GetFileNameWithoutExtension(modelPath);
-				mstudiomodelv54_t model = *reinterpret_cast<mstudiomodelv54_t*>(pBodyPart + bodyPart.modelindex + (j * sizeof(mstudiomodelv54_t)));
+				mstudiomodelv54_t model = *reinterpret_cast<mstudiomodelv54_t*>(pBodyPart + bodyPart->modelindex + (j * sizeof(mstudiomodelv54_t)));
 				
 				if (!*model.name)
 					qc.Write("\tblank\n");
@@ -48,26 +48,26 @@ void RpakLib::ExportQC(int assetVersion, const string& Path, const string& model
 		}
 
 		qc.WriteFmt("$texturegroup \"skinfamilies\"\n{\n");
-		for (int i = 0; i < hdr.numskinfamilies; ++i)
+		for (int i = 0; i < hdr->numskinfamilies; ++i)
 		{
-			char* pSkinFamily = rmdlBuf + hdr.skinindex + (i * hdr.numskinref * sizeof(short));
+			char* pSkinFamily = rmdlBuf + hdr->skinindex + (i * hdr->numskinref * sizeof(short));
 
 			char* skinName = (char*)"default";
 
 			if (i > 0)
 			{
-				int* pSkinNameIndex = reinterpret_cast<int*>(rmdlBuf + hdr.skinindex + (hdr.numskinfamilies * hdr.numskinref * sizeof(short)) + ((i-1) * sizeof(int)));
+				int* pSkinNameIndex = reinterpret_cast<int*>(rmdlBuf + hdr->skinindex + (hdr->numskinfamilies * hdr->numskinref * sizeof(short)) + ((i-1) * sizeof(int)));
 				skinName = rmdlBuf + *pSkinNameIndex;
 			}
 
 			qc.WriteFmt("\t\"%s\" { ", skinName);
-			for (int j = 0; j < hdr.numskinref; ++j)
+			for (int j = 0; j < hdr->numskinref; ++j)
 			{
 				// texture index
 				short texId = *reinterpret_cast<short*>(pSkinFamily + (j * sizeof(short)));
 
 				// pointer to texture entry
-				char* pTexture = rmdlBuf + hdr.textureindex + (texId * sizeof(mstudiotexturev54_t));
+				char* pTexture = rmdlBuf + hdr->textureindex + (texId * sizeof(mstudiotexturev54_t));
 
 				// texture entry
 				mstudiotexturev54_t texture = *reinterpret_cast<mstudiotexturev54_t*>(pTexture);
@@ -81,58 +81,58 @@ void RpakLib::ExportQC(int assetVersion, const string& Path, const string& model
 		}
 		qc.Write("}\n\n");
 
-		if (hdr.numlocalattachments)
+		if (hdr->numlocalattachments)
 			qc.Write("// !!! attachment rotation angles may be wrong !!!\n");
-		for (int i = 0; i < hdr.numlocalattachments; ++i)
+		for (int i = 0; i < hdr->numlocalattachments; ++i)
 		{
 			// get attachment
-			char* pAttachment = rmdlBuf + hdr.localattachmentindex + (i * sizeof(mstudioattachmentv54_t));
-			mstudioattachmentv54_t attachment = *reinterpret_cast<mstudioattachmentv54_t*>(pAttachment);
+			char* pAttachment = rmdlBuf + hdr->localattachmentindex + (i * sizeof(mstudioattachmentv54_t));
+			mstudioattachmentv54_t* attachment = reinterpret_cast<mstudioattachmentv54_t*>(pAttachment);
 
-			char* attachmentName = pAttachment + attachment.sznameindex;
+			char* attachmentName = pAttachment + attachment->sznameindex;
 
 			// get attachment's bone
-			char* pBone = rmdlBuf + hdr.boneindex + (attachment.localbone * sizeof(mstudiobonev54_t));
-			mstudiobonev54_t bone = *reinterpret_cast<mstudiobonev54_t*>(pBone);
+			char* pBone = rmdlBuf + hdr->boneindex + (attachment->localbone * sizeof(mstudiobonev54_t));
+			mstudiobonev54_t* bone = reinterpret_cast<mstudiobonev54_t*>(pBone);
 
-			char* boneName = pBone + bone.sznameindex;
+			char* boneName = pBone + bone->sznameindex;
 
-			Vector3 angles = attachment.localmatrix.GetRotationMatrixAsDegrees();
+			Vector3 angles = attachment->localmatrix.GetRotationMatrixAsDegrees();
 
 			qc.WriteFmt("$attachment \"%s\" \"%s\" %f %f %f rotate %f %f %f\n",
 				attachmentName,
 				boneName,
-				attachment.localmatrix.c3r0, attachment.localmatrix.c3r1, attachment.localmatrix.c3r2,
+				attachment->localmatrix.c3r0, attachment->localmatrix.c3r1, attachment->localmatrix.c3r2,
 				angles.X, angles.Y, angles.Z
 			);
 		}
 		qc.Write("\n");
 
-		for (int i = 0; i < hdr.numhitboxsets; ++i)
+		for (int i = 0; i < hdr->numhitboxsets; ++i)
 		{
-			char* pHitboxSet = rmdlBuf + hdr.hitboxsetindex + (i * sizeof(mstudiohitboxset_t));
-			mstudiohitboxset_t hitboxSet = *reinterpret_cast<mstudiohitboxset_t*>(pHitboxSet);
+			char* pHitboxSet = rmdlBuf + hdr->hitboxsetindex + (i * sizeof(mstudiohitboxset_t));
+			mstudiohitboxset_t* hitboxSet = reinterpret_cast<mstudiohitboxset_t*>(pHitboxSet);
 
 			// get hboxset name
-			char* hitboxSetName = pHitboxSet + hitboxSet.sznameindex;
+			char* hitboxSetName = pHitboxSet + hitboxSet->sznameindex;
 
 			qc.WriteFmt("$hboxset \"%s\"\n\n", hitboxSetName);
 
-			for (int j = 0; j < hitboxSet.numhitboxes; ++j)
+			for (int j = 0; j < hitboxSet->numhitboxes; ++j)
 			{
-				char* pHitbox = pHitboxSet + hitboxSet.hitboxindex + (j*sizeof(mstudiobboxv54_t));
-				mstudiobboxv54_t hitbox = *reinterpret_cast<mstudiobboxv54_t*>(pHitbox);
+				char* pHitbox = pHitboxSet + hitboxSet->hitboxindex + (j*sizeof(mstudiobboxv54_t));
+				mstudiobboxv54_t* hitbox = reinterpret_cast<mstudiobboxv54_t*>(pHitbox);
 
 				// get bone name
-				char* pBone = rmdlBuf + hdr.boneindex + (hitbox.bone * sizeof(mstudiobonev54_t));
-				mstudiobonev54_t bone = *reinterpret_cast<mstudiobonev54_t*>(pBone);
-				char* boneName = pBone + bone.sznameindex;
+				char* pBone = rmdlBuf + hdr->boneindex + (hitbox->bone * sizeof(mstudiobonev54_t));
+				mstudiobonev54_t* bone = reinterpret_cast<mstudiobonev54_t*>(pBone);
+				char* boneName = pBone + bone->sznameindex;
 
 				qc.WriteFmt("$hbox %i \"%s\" %f %f %f %f %f %f\n",
-					hitbox.group,
+					hitbox->group,
 					boneName,
-					hitbox.bbmin.X, hitbox.bbmin.Y, hitbox.bbmin.Z,
-					hitbox.bbmax.X, hitbox.bbmax.Y, hitbox.bbmax.Z
+					hitbox->bbmin.X, hitbox->bbmin.Y, hitbox->bbmin.Z,
+					hitbox->bbmax.X, hitbox->bbmax.Y, hitbox->bbmax.Z
 				);
 			}
 			qc.Write("\n");
